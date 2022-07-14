6fc4dd2191ac2c6089a921337b5408a2aa299e35
emicludo <86956780+emicludo@users.noreply.github.com>
2022-07-14 14:22:27 +0200

    implement changes in interface models

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -3,20 +3,30 @@
 Am I using Record utility type correctly?
 I only added so I can run this line: newSeqItem[e.target.name] = e.target.value on InputGroup
 But I don't fully understand its implicance 
-*/
-export interface ISequenceItem extends Record<string,any>{
+ 
+export interface ISequenceItem2 extends Record<string,any>{
     id: number
     name: string
     required: string[]
-    selector?: string
-    url?: string
-    value?: string
+    selector: string
+    url: string
+    value: string
+} */
+
+export interface ISequenceItem {
+    [key: string]: number | string | string[]
+    id: number
+    name: string
+    required: string[]
+    selector: string
+    url: string
+    value: string
 }
 
 export interface IResultSequenceItem {
-    0?: string;
-    1?: string;
-    2?: string;
+    0: eventTypeEnum;
+    1: string;
+    2: string;
 }
 
 export interface ILogEvent {
@@ -26,20 +36,26 @@ export interface ILogEvent {
 }
 
 export interface IEvent {
-    name: EeventTypeEnum
-    required: EeventTypeEnum[]  //@Josh, I know this is not implemented correctly, but my intention was to set 
+    name: eventTypeEnum
+    required: inputFieldEnum[]  //@Josh, I know this is not implemented correctly, but my intention was to set 
 }                               // only the values on the enumerator as valid for this field. How can I do that?
+export interface IScreenshotResponse {
+    error: string;
+    log?: ILogEvent[];
+    screenshot?: string;
+}
 
-export enum EeventTypeEnum {
+export enum eventTypeEnum {
     Navigate = "navigate",
     SetValue = "setValue",
     Click = "click",
     Submit = "submit",
     WaitUntil = "waitUntil",
+    Invalid = "invalid",
 }
 
-export interface IScreenshotResponse {
-    error: string;
-    log?: ILogEvent[];
-    screenshot?: string;
+export enum inputFieldEnum {
+    Selector = "selector",
+    Value = "value",
+    URL = "url",
 }
